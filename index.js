d8dc7bd1bfba6459d113d33ba7f16f87ee313d7c
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-23 09:45:32 +0530

    js4

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
  var fruits =['apple', 'banana','orange'];
- console.log(fruits.join());
+ console.log(fruits.join());//apple,banana,orange
  var num=[1,2,3,4,5];
- console.log(num.join("-"));
+ console.log(num.join("-"));//1-2-3-4-5
  var hel=['h','e','l','l','o'];
- console.log(hel.join(""));
+ console.log(hel.join(""));//hello
  var js='javascript is good';
- console.log(js.slice(0,10));
+ console.log(js.slice(0,10));//javascript
  var sen='i love playing';
- console.log(sen.slice(7,14));
+ console.log(sen.slice(7,14));//playing
  var text='frontend is good';
- console.log(text.slice(-1.-3));
+ console.log(text.slice(-1.-3));//good
  var date ='2024-10-22';
- console.log(date.split("-"));
+ console.log(date.split("-"));//[ '2024', '10', '22' ]
  var java ='i love html';
  var jav=java.split(" ");
 var jav1=jav.slice(0,2);
-console.log(jav1);
+console.log(jav1);//[ 'i', 'love' ]
 
  
  
