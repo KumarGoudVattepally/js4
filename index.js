9ce173c6ffff8067e604029af555437cb554ea68
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-23 09:38:33 +0530

    hj

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
- var fruits =["apple", "banana","orange"];
+ var fruits =['apple', 'banana','orange'];
  console.log(fruits.join());
+ var num=[1,2,3,4,5];
+ console.log(num.join("-"));
+ var hel=['h','e','l','l','o'];
+ console.log(hel.join(""));
+ var js='javascript is good';
+ console.log(js.slice(0,10));
+ var sen='i love playing';
+ console.log(sen.slice(7,14));
+ var text='frontend is good';
+ console.log(text.slice(-1.-3));
+ var date ='2024-10-22';
+ console.log(date.split("-"));
+ var java ='i love html';
+ var jav=java.split(" ");
+var jav1=jav.slice(0,2);
+console.log(jav1);
+
+ 
+ 
+
+
+ 
+
+ 
+ 
+ 
